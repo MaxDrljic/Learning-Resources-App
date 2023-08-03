4463844f198042eeb16c6c2f89d4ff91746768fe
Max Drljic <max.drljic@advisera.com>
2023-08-03 19:08:07 +0200

    Created basic structure & added dummy data

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: "official-guide", 
+                    title: "Official Guide",
+                    description: "The official Vue.js documentation.",
+                    link: "https://vuejs.org/guide/introduction.html"
+                },
+                {
+                    id: "google", 
+                    title: "Google",
+                    description: "Learn how to google the right way!",
+                    link: "https://www.google.org/"
+                }
+            ]
+        }
+    },
+};
+</script>
